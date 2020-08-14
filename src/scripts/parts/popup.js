293669ddbb2a98729ca61f1d48f97ad12a2f79fa
Adelia <adelgants@gmail.com>
2020-08-14 03:39:39 +0300

    add popup

---
+++ src/scripts/parts/popup.js
@@ -0,0 +1,7 @@
+export default function popUp() {
+  const $ = require('jquery');
+  require('magnific-popup');
+  $('.popup').magnificPopup({
+    type: 'inline'
+  });
+}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,6 @@
-console.log(5);
+const $ = require('jquery');
+import popUp from './parts/popup';
+
+$(document).ready(() => {
+  popUp();
+});
