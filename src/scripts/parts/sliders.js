8751da258b8e394cf594cb90cfd9f95d3a8dbbbf
Adelia <adelgants@gmail.com>
2020-08-14 18:32:07 +0300

    add home-slider

--- src/scripts/parts/sliders.js
+++ src/scripts/parts/sliders.js
@@ -3,9 +3,25 @@ export default function initSliders() {
   require('slick-carousel');
 
   $('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
     centerMode: true,
-    arrows: true
+    infinite: true,
+    arrows: true,
+    variableWidth: true
+  });
+  $('.slider').on('beforeChange', function (currentSlide, slick, nextSlide) {
+    switch (nextSlide) {
+      case 2:
+        $('header').removeAttr('class');
+        $('header').addClass('white');
+        break;
+      case 0:
+        $('header').removeAttr('class');
+        $('header').addClass('dark');
+        break;
+      case 1:
+        $('header').removeAttr('class');
+        $('header').addClass('dark');
+        break;
+    }
   });
 };
