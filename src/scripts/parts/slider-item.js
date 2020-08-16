3834a76698d3465c876f8fea746b107ea45c96e3
Adelia <adelgants@gmail.com>
2020-08-16 05:59:02 +0300

    add slider-item

--- src/scripts/parts/slider-item.js
+++ src/scripts/parts/slider-item.js
@@ -1,3 +1,21 @@
 export default function initSliderItem() {
-  ;
-};
+  const $ = require('jquery');
+  require('slick-carousel');
+
+  $('.slider-item-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider-item-nav'
+  });
+
+  $('.slider-item-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-item-main',
+    focusOnSelect: true,
+    arrows: false,
+    useTransform: false
+  });
+}
+
